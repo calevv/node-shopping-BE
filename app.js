66380ec0803a95f1cbab41b2d19d6b70dbeb98e8
calevv <ika818488@gmail.com>
2025-07-31 10:26:19 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const MONGODB_URI_SHOP = process.env.MONGODB_URI_SHOP;
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose
+  .connect(MONGODB_URI_SHOP, { useNewUrlParser: true })
+  .then(() => {
+    console.log("mongoose connectes");
+  })
+  .catch((err) => {
+    console.log("DB connecton fail", err);
+  });
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server on ");
+});
---
+++ models/Cart.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const User = require("./User");
+const Product = require("./Product");
+const Schema = mongoose.Schema;
+
+const cartSchema = Schema(
+  {
+    userId: { type: mongoose.objectId, ref: User },
+    items: [
+      {
+        type: mongoose.objectId,
+        ref: Product,
+        size: { type: String, required: true },
+        qty: { type: Number, default: 1, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+cartSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.createdAt;
+  return obj;
+};
+const Cart = mongoose.model("Cart", cartSchema);
+
+module.exports = Cart;
