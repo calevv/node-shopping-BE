e06728931ca0c68fa03056b31e6c6bcbba7a8f61
jeong-minG <ika818488@gmail.com>
2025-08-02 23:05:13 +0900

    fix:useNewUrlParser제거

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const indexRouter = require("./routes/index");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const indexRouter = require('./routes/index');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const MONGODB_URI_SHOP = process.env.MONGODB_URI_SHOP;
@@ -12,16 +12,16 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use("/api", indexRouter);
+app.use('/api', indexRouter);
 
 mongoose
-  .connect(MONGODB_URI_SHOP, { useNewUrlParser: true })
-  .then(() => {
-    console.log("mongoose connectes");
-  })
-  .catch((err) => {
-    console.log("DB connecton fail", err);
-  });
+    .connect(MONGODB_URI_SHOP)
+    .then(() => {
+        console.log('mongoose connectes');
+    })
+    .catch((err) => {
+        console.log('DB connecton fail', err);
+    });
 app.listen(process.env.PORT || 5000, () => {
-  console.log("server on ");
+    console.log('server on ');
 });
