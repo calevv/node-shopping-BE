af28e45fdc7159f29491d4688b8c5d5901c49648
jeong-minG <ika818488@gmail.com>
2025-08-03 18:27:48 +0900

    feat:token로그인

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,36 +1,47 @@
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 const saltRounds = 10;
 const userController = {};
 
 userController.createUser = async (req, res) => {
-  try {
-    let { name, email, password, level } = req.body;
+    try {
+        let { name, email, password, level } = req.body;
 
-    const user = await User.findOne({ email });
-    if (!name || !email || !password) {
-      throw new Error("이름, 이메일, 비밀번호는 필수 입력 항목입니다.");
+        const user = await User.findOne({ email });
+        if (!name || !email || !password) {
+            throw new Error('이름, 이메일, 비밀번호는 필수 입력 항목입니다.');
+        }
+
+        if (user) {
+            throw new Error('이미 가입한 유저입니다.');
+        }
+
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hash = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            name,
+            email,
+            password: hash,
+            level: level ? level : 'customer',
+        });
+        await newUser.save();
+        res.status(200).json({ status: 'success', data: newUser });
+    } catch (err) {
+        res.status(400).json({ status: 'fail', error: err.message });
     }
-
-    if (user) {
-      throw new Error("이미 가입한 유저입니다.");
-    }
-
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hash = await bcrypt.hash(password, salt);
-
-    const newUser = new User({
-      name,
-      email,
-      password: hash,
-      level: level ? level : "customer",
-    });
-    await newUser.save();
-    res.status(200).json({ status: "ok", data: newUser });
-  } catch (err) {
-    res.status(400).json({ status: "fail", error: err.message });
-  }
 };
-
+userController.getUser = async (req, res) => {
+    try {
+        const { userId } = req;
+        const user = await User.findById(userId);
+        if (user) {
+            return res.status(200).json({ status: 'success', user });
+        }
+        throw new Error('Invalid token');
+    } catch (err) {
+        return res.status(400).json({ status: 'error', error: err.message });
+    }
+};
 module.exports = userController;
--- routes/user.api.js
+++ routes/user.api.js
@@ -1,8 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const userController = require("../controller/user.controller");
+const userController = require('../controller/user.controller');
+const authController = require('../controller/auth.controller');
 
 // 1. 회원가입 endpoint
-router.post("/", userController.createUser);
+router.post('/', userController.createUser);
+// 2. 토근 로그인 endpoint
+router.get('/me', authController.authenticate, userController.getUser); // token이 valid한 값인지, token으로 user를 찾아서 리턴
 
 module.exports = router;
