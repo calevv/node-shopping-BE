da299b04e5f89f9d031de5307ba00595c2cbc2f5
calevv <ika818488@gmail.com>
2025-08-05 17:27:29 +0900

    feat:상품보여주기

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,17 +1,47 @@
-const Product = require('../models/Product');
+const Product = require("../models/Product");
 
 const productController = {};
 
 productController.createProduct = async (req, res) => {
-    try {
-        const { sku, name, image, price, category, description, stock, status } = req.body;
-        const product = new Product({ sku, name, image, price, category, description, stock, status });
+  try {
+    const { sku, name, image, price, category, description, stock, status } =
+      req.body;
+    const product = new Product({
+      sku,
+      name,
+      image,
+      price,
+      category,
+      description,
+      stock,
+      status,
+    });
 
-        await product.save();
-        return res.status(200).json({ status: 'success', product });
-    } catch (err) {
-        return res.status(400).json({ status: 'error', error: err.message });
-    }
+    await product.save();
+    return res.status(200).json({ status: "success", product });
+  } catch (err) {
+    return res.status(400).json({ status: "error", error: err.message });
+  }
+};
+
+productController.getProducts = async (req, res) => {
+  try {
+    const { page, name } = req.query;
+
+    const cond = name
+      ? {
+          name: { $regex: name, $options: "i" },
+        }
+      : {};
+
+    let query = Product.find(cond);
+
+    const productList = await query.exec();
+
+    return res.status(200).json({ status: "success", data: productList });
+  } catch (err) {
+    return res.status(400).json({ status: "error", error: err.message });
+  }
 };
 
 module.exports = productController;
--- routes/product.api.js
+++ routes/product.api.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const productController = require('../controller/product.controller');
-const authController = require('../controller/auth.controller');
+const productController = require("../controller/product.controller");
+const authController = require("../controller/auth.controller");
 
-router.post('/', authController.authenticate, authController.checkAdminPermission, productController.createProduct);
+router.post(
+  "/",
+  authController.authenticate,
+  authController.checkAdminPermission,
+  productController.createProduct
+);
+router.get("/", productController.getProducts);
 
 module.exports = router;
